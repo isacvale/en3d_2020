892e050c5eb6c555e7b5bdff1e929e1305862d17
isacvale <isac.dvo@gmail.com>
2020-09-07 11:26:34 -0300

    Testing from submission by code.

--- src/pages/_templates/components/Footer.js
+++ src/pages/_templates/components/Footer.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Footer.css'
 
 const Footer = props => {
+  const [nome, setNome]= useState('')
+  const [email, setEmail]= useState('')
+  const [mensagem, setMensagem]= useState('')
+
+  const encode = data =>
+    Object.entries(data)
+      .map(entry => `${encodeURIComponent(entry[0])}=${encodeURIComponent(entry[1])}`)
+      .join('&')
+
   return (
     <section className='Footer'>
       <h1 data-inverted>Entre em contato</h1>
@@ -19,20 +28,54 @@ const Footer = props => {
 
         <label>
           <span className='Footer__FormLabel'>Seu nome:</span>
-          <input className='Footer__FormInput' type='text' name='nome' />
+          <input
+            className='Footer__FormInput'
+            type='text'
+            name='nome'
+            value={nome}
+            onChange={ev => setNome(ev.target.value)}
+          />
         </label>
 
         <label>
           <span className='Footer__FormLabel'>Seu email:</span>
-          <input className='Footer__FormInput' type='email' name='email' />
+          <input
+            className='Footer__FormInput'
+            type='email' 
+            name='email'
+            value={email}
+            onChange={ev => setEmail(ev.target.value)}
+          />
         </label>
 
         <label>
           <span className='Footer__FormLabel'>Sua mensagem:</span>
-          <textarea className='Footer__FormInput' name='mensagem' />
+          <textarea
+            className='Footer__FormInput'
+            name='mensagem'
+            value={mensagem}
+            onChange={ev => setMensagem(ev.target.value)}
+          />
         </label>
 
         <button type="submit">Enviar</button>
+        <button
+          type="button"
+          onClick={ev => fetch('/', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: encode({
+              'form-name': 'contato',
+              nome,
+              email,
+              mensagem
+            })
+          })}
+        >
+          Via code
+        </button>
       </form>
     </section>
   )
